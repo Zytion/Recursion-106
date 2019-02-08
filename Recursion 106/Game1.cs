814523bf01740b677293934f203c52a8889b8a06
Bobby Levin <bil1616@ad.rit.edu>
2019-02-08 09:33:30 -0500

    Added Project Files

--- Recursion 106/Game1.cs
+++ Recursion 106/Game1.cs
@@ -12,6 +12,9 @@ namespace Recursion_106
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D image;
+        SpriteFont font;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -40,7 +43,9 @@ namespace Recursion_106
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            //Load Content
+            image = Content.Load<Texture2D>("pepe");
+            font = Content.Load<SpriteFont>("File");
         }
 
         /// <summary>
@@ -76,8 +81,51 @@ namespace Recursion_106
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            //Begin the recursive drawing and store the count
+            int count = DrawRecursiveThing(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, Color.White);
+
+            //Draw the count on the screen
+            spriteBatch.DrawString(font, "Count: " + count, new Vector2(GraphicsDevice.Viewport.Width - 100, 0), Color.Black);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+        /// <summary>
+        /// Draws the image and swaps the color between white and orange, 
+        /// then it shrinks the image by half and redraws it in the top and bottom corners.
+        /// Continues until the image gets smaller than 20 pixels
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        int DrawRecursiveThing(int x, int y, int width, int height, Color color)
+        {
+            //Draw the image
+            spriteBatch.Draw(image, new Rectangle(x, y, width, height), color);
+
+            //Return if image is below 20 X 20 pixels
+            if (width <= 20 || height <= 20)
+                return 1;
+
+            //Invert the colors
+            if (color == Color.White)
+                color = Color.Orange;
+            else
+                color = Color.White;
+
+            //Half the image size
+            width /= 2;
+            height /= 2;
+
+            //Call the method twice and add 1 to the return
+            return 1 + DrawRecursiveThing(x,y,width, height, color) + 
+                DrawRecursiveThing(x + width, y + height, width, height, color);
         }
     }
 }
